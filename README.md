43bff388a1e78cfb5f162b8de8ef1d463a62388e
Nwachee <fuzzy245.in@gmail.com>
2025-07-26 19:58:06 +0100

    Feat: ERC20 with OpenZepplin

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ERC2OWithFoundry
 A simple implementation of the ERC20 token standard using Solidity and tested with Foundry
+
---
+++ src/testToken.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract test {
+    mapping (address => uint256) public s_balances;
+
+      function name() public pure returns(string memory) {
+        return "Test Token";
+    }
+
+    function totalSupply() public pure returns (uint256){
+        return 100 ether; // 100000000000000000000
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256){
+        return s_balances[_owner];
+    }
+}
